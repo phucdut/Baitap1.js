d87ae74f588ea1e69681d40a3c7e3381397f1148
unknown <phuckarry634@gmail.com>
2022-01-15 17:09:51 +0700

    Bài tâp 1(Bài 1)

---
+++ index01.js
@@ -0,0 +1,9 @@
+let day;
+let month;
+let year;
+inputDay = prompt("Enter the  day");
+inputMonth = prompt("Enter the month");
+inputYear = prompt("Enter the year");
+
+alert(" " + inputDay + "-" + inputMonth + "-" + inputYear);
+document.write(" " + inputDay + "-" + inputMonth + "-" + inputYear);
